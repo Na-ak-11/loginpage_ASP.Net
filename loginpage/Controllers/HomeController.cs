7fb7d27b40ef2c23dae91b76d245501e5a1b3d5c
WESTDIST <WESTDIST@LAPTOP-GP6SB9LE>
2022-06-22 07:14:41 +0530

    Add project files.

---
+++ loginpage/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using loginpage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loginpage.Controllers
+{
+    public class HomeController : Controller
+    {
+        loginEntities db = new loginEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user log)
+        {
+           var user = db.users.Where(x => x.username == log.username && x.password == log.password).Count();
+
+            if (user > 0)
+            {
+                return RedirectToAction("successful");
+            }
+            else
+            {
+                return RedirectToAction("error");
+            }
+            
+        }
+        public ActionResult successful()
+        {
+            return View();
+        }
+
+        public ActionResult error()
+        {
+            return View();
+        }
+
+    }
+}
